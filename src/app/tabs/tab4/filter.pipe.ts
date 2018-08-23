a9a135d13d920b32455e5ae7465fcf5381803d49
Vaibhav Kumar <vaibhav.kumar@gfs.com>
2018-08-23 14:49:56 +0530

    EET-5.3 Filter option

--- src/app/tabs/tab4/filter.pipe.ts
+++ src/app/tabs/tab4/filter.pipe.ts
@@ -6,9 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, searchString: string, propName: string): any {
-    if (value.length === 0) {
-      return value;
-    }
     const resultArray = [];
     for (const item of value) {
       if (item[propName] === searchString) {
